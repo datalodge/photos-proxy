87b3303ee6579d91fd9a495242f7ec73fcb40edc
paul-do <pauldo247@gmail.com>
2019-03-06 20:21:51 -0800

    Implement progress - proxy not properly stress testing

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
+const newrelic = require('newrelic')
 const express = require('express');
-//const bodyParser = require('body-parser');
+const proxy = require('http-proxy-middleware');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(express.static(__dirname + '/public'));
+app.use(cors());
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(
+  '/pictures',
+  proxy({
+    target: 'http://localhost:3001',
+    changeOrigin: true
+  })
+);
+
+app.use('/', express.static(__dirname + '/public'));
+
+
 
 app.listen(port, () => {
   console.log(`I'm serving from http://localhost:${port}`);
